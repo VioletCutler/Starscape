b9bbd38766d5d49436915e3ab0dc80701b51da38
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2023-08-13 18:21:47 -0400

    connected client and server through websockets

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
-const express = require ('express')
-const app = express()
+const Express = require ('express')
+const app = Express()
 const path = require('path')
 const socket = require('socket.io')
+const cors = require('cors')
+const morgan = require('morgan')
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use(cors())
+
+app.use(morgan('dev'))
+app.use(Express.json())
+
+app.use('*', (req, res, next) => {
+  console.log('A request was made to the server');
+  next()
+})
+
+app.use(Express.static(path.join(__dirname, '..', 'public')))
+
+
+app.get('/', (req, res, next) => {
+  res.send({
+    success: true,
+    message: 'You have connected to the server!'
+  })
+})
 
 const PORT = 3000
 const server = app.listen(PORT, () => {
@@ -14,4 +35,9 @@ const serverSocket = socket(server)
 
 serverSocket.on('connection', (socket) => {
   console.log(`Connection from client ${socket.id}`)
+  socket.emit("hello from server", 1, "2", {3: Buffer.from([4])})
+
+  socket.on("hello from client", () => {
+    console.log('A message was received from the client')
+  })
 })
